faa0673fcdf4827ff724402b17129a8d49ab763c
Matheus Viana <vianamp@gmail.com>
2018-11-26 17:24:10 -0200

    New dataset for minipipeline

--- data-scripts/process.R
+++ data-scripts/process.R
@@ -6,7 +6,7 @@ library(gridExtra)
 
 ROOT_FOLDER <- "/Users/viana/projects/qcb/data-raw/"
 
-DATASET_NAME <- "NUCLEUS_CARDIO"
+DATASET_NAME <- "NUCLEUS_HIPS"
 
 setwd(ROOT_FOLDER)
 
@@ -35,6 +35,14 @@ Table_raw <- within(Table_raw, dna_intensity_sum <- dna_volume*dna_intensity_mea
 Table_raw <- within(Table_raw, dna_bright_spots_intensity_mean_norm <- dna_bright_spots_intensity_mean/dna_intensity_mean)
 Table_raw <- within(Table_raw, dna_bright_spots_xy_rad <- sqrt(dna_bright_spots_xy_cross_sec_area_mean/pi))
 
+Table_raw$date <- unlist(lapply(Table_raw$czi, function(x) strsplit(as.character(x),"_")[[1]][1]))
+Table_raw$date <- paste("D", Table_raw$date, sep="")
+
+should_not_be_used <- c("20180924_R07.czi","20180927_R04.czi","20180927_R05.czi","20180928_R03.czi","20180928_R04.czi",
+                        "20181106_J01_020_Out.czi", "20181106_J02_015_Out.czi", "20181106_J03_015_Out.czi")
+
+Table_raw <- Table_raw[is.na(match(Table_raw$czi, should_not_be_used)),]
+
 #
 # Outliers detection
 #
@@ -62,13 +70,18 @@ detect_outliers <- function(Table_or, fac_var, num_var, nsigma) {
   return(Table_or$tmp_outlier)
 }
 
-# out1 <- detect_outliers(Table_or=Table_raw, fac_var="condition", num_var="dna_volume", nsigma=2)
-# out2 <- detect_outliers(Table_or=Table_raw, fac_var="condition", num_var="dna_intensity_max", nsigma=2)
-# out2 <- detect_outliers(Table_or=Table_raw, fac_var="condition", num_var="dna_roundness", nsigma=2)
+Table <- Table_raw
+Table$outlier <- FALSE
 
-Table_raw$outlier <- out1 + out2
+out1 <- detect_outliers(Table_or=Table, fac_var="condition", num_var="dna_volume", nsigma=2)
+out2 <- detect_outliers(Table_or=Table, fac_var="group", num_var="dna_intensity_max", nsigma=2)
+out3 <- detect_outliers(Table_or=Table, fac_var="condition", num_var="dna_roundness_roughness_xy", nsigma=3)
+Table$outlier <- ifelse(out1+out2+out3>0, "yes", "no")
+#Table$outlier <- ifelse(out2>0, "yes", "no")
 
-Table <- subset(Table_raw, outlier==0)
+Table <- subset(Table, outlier=="no")
+
+#Table <- Table[-as.numeric(which(apply(is.na(Table),MARGIN = 1,FUN = sum)>0)),]
 
 #
 # Save processed table
@@ -76,6 +89,4 @@ Table <- subset(Table_raw, outlier==0)
 
 write.table(x=Table, file=paste(ROOT_FOLDER,"../engine/data-processed/",DATASET_NAME,".csv",sep=""), row.names=F, sep=";")
 
-ggplot(Table_raw) + geom_point(aes(dna_volume,dna_roundness_roughness_xy,col=outlier))
-
-names(Table_raw)
+ggplot(Table) + geom_point(aes(dna_volume,dna_intensity_mean,col=outlier))
