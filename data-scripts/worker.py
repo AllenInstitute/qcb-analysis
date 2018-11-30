8432b6824403262a5e042321987114c1835b2092
Matheus Viana <matheus.viana@alleninstitute.org>
2018-11-30 03:05:31 -0800

    Runing final data -- 10% done

--- data-scripts/worker.py
+++ data-scripts/worker.py
@@ -126,12 +126,12 @@ if __name__ == "__main__":
                 else:
 
                     series_id = position["name"]
-                    loc = series_id.find("Scene")
 
                     # Parse series ID in the segmentation name
                     # First positiuon is 0, alhtough it is shown as Scene-01
-                    series_id = 0
                     if config_czi["force_single_scene"] == "yes":
+                        series_id = 0
+                    else:
                         series_id = int(series_id.split("Scene")[1].split("-")[1]) - 1
 
                 # Structure channel
